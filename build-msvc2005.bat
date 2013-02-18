56e428d3b7b3634e32a4963a1f14ecf574324e9f
Stanisław Halik <sthalik@misaki.pl>
2013-02-18 15:26:51 +0100

    New Win32 build system

---
+++ build-msvc2005.bat
@@ -0,0 +1,6 @@
+call "c:\Program Files\Microsoft Visual Studio 8\VC\vcvarsall.bat"
+set INCLUDE=%INCLUDE%;c:\Program Files\Microsoft SDKs\Windows\v6.0A\Include
+set LIB=%LIB%;c:\Program Files\Microsoft SDKs\Windows\v6.0A\lib
+set PATH=%PATH%;C:\Program Files\Microsoft SDKs\Windows\v6.0A\bin
+cmake .
+cmake --build . --config release --target install --
--- upload-snapshot.sh
+++ upload-snapshot.sh
@@ -7,26 +7,29 @@ touch ./last-snapshot.rev
 read line < ./last-snapshot.rev
 LASTREV="$line"
 CURREV="$(git rev-parse HEAD)"
+foo=0
 
 export PATH=/opt/wine.git/bin:"$PATH" WINEPREFIX=$HOME/dev/msvc WINEDEBUG=-all
+ORIGIN="$(pwd)"
 
 wineserver -k
 
 if test "$CURREV" != "$LASTREV"; then
-	echo $CURREV > ./last-snapshot.rev
 	cd $HOME/dev/msvc/drive_c/build/ftnoir-posix || exit 1
 	wine $WINEPREFIX/drive_c/Program\ Files/Microsoft\ Visual\ Studio\ 9.0/Common7/IDE/mspdbsrv.exe -start -spawn -shutdowntime -1 &
 	sleep 0.5
 	rm -rf ./install/
-	wine cmd /k $HOME/dev/facetracknoir-posix/compile-msvc.bat
+	wine cmd /k "$ORIGIN/build-msvc2008.bat" || exit 1
 	pushd $HOME/dev/msvc/drive_c/build/ftnoir-faceapi || exit 1
 	rm -rf ./install/
-	wine cmd /k $HOME/dev/facetracknoir-posix/compile-msvc80.bat
+	wine cmd /k "$ORIGIN/build-msvc2005.bat" || exit 1
 	rsync -ra ./install/faceapi $HOME/dev/msvc/drive_c/build/ftnoir-posix/install || exit 1
 	popd || exit 1
 	FNAME="ftnoir-posix-$(date +%Y%m%d-%H_%M_%S)"-"$CURREV".7z
-	7z -y a -t7z -m0=lzma -mx=9 -mfb=128 -md=8m -ms=on "$FNAME" install || exit 1
-	ssh -4o BatchMode=yes "$USER"@"$BACKUPHOST" find /var/www/ftnoir/ -type f -mtime 60 -delete
+	7z -y a -t7z -m0=lzma -mx=9 -mfb=128 -md=8m -ms=on "$FNAME" install && foo=1 || exit 1
+	ssh -4o BatchMode=yes "$USER"@"$BACKUPHOST" find /var/www/ftnoir/ -type f -mtime 14 -delete
 	scp -4o BatchMode=yes "$FNAME" "$USER"@"$BACKUPHOST":/var/www/ftnoir/
 	rm *.7z
+	test $foo && echo $CURREV > ./last-snapshot.rev
+	wineserver -k
 fi
