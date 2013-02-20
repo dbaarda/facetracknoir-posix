204253edada07f74e014f3045afc3446fff96f10
Stanisław Halik <sthalik@misaki.pl>
2013-02-20 19:40:41 +0100

    New upload script using a Windows VM (surprisingly, works)

--- build-msvc2008.bat
+++
@@ -1,6 +0,0 @@
-call "c:\Program Files\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
-set INCLUDE=%INCLUDE%;c:\Program Files\Microsoft SDKs\Windows\v6.0A\Include
-set LIB=%LIB%;c:\Program Files\Microsoft SDKs\Windows\v6.0A\lib
-set PATH=%PATH%;C:\Program Files\Microsoft SDKs\Windows\v6.0A\bin
-cmake .
-cmake --build . --config release --target install --
--- upload-snapshot.sh
+++ upload-snapshot.sh
@@ -6,29 +6,16 @@ cd $HOME/dev/facetracknoir-posix || exit 1
 touch ./last-snapshot.rev
 read LASTREV < ./last-snapshot.rev
 CURREV="$(git rev-parse HEAD)"
-foo=0
-
-export WINEPREFIX=$HOME/dev/msvc WINEDEBUG=-all
+TMP=/tmp
+WINHOST=Administrator@172.18.2.69
+WINCMD=/home/Administrator/build-ftnoir.sh
+WINPATH=/C/build/ftnoir-posix-msvc2010/install.7z
+WINSH=/mingw/msys/1.0/bin/sh
+NEKO=/mingw/msys/1.0/bin/cat
 ORIGIN="$(pwd)"
+FNAME="ftnoir-posix-$(date +%Y%m%d-%H_%M_%S)"-"$CURREV".7z
 
-wineserver -k
-
-if test "$CURREV" != "$LASTREV"; then
-	cd $HOME/dev/msvc/drive_c/build/ftnoir-posix || exit 1
-	wine $WINEPREFIX/drive_c/Program\ Files/Microsoft\ Visual\ Studio\ 9.0/Common7/IDE/mspdbsrv.exe -start -spawn -shutdowntime -1 &
-	sleep 0.5
-	rm -rf ./install/
-	wine cmd /k "$ORIGIN/build-msvc2008.bat" </dev/null || exit 1
-	pushd $HOME/dev/msvc/drive_c/build/ftnoir-faceapi || exit 1
-	rm -rf ./install/
-	wine cmd /k "$ORIGIN/build-msvc2005.bat" </dev/null || exit 1
-	rsync -ra ./install/faceapi $HOME/dev/msvc/drive_c/build/ftnoir-posix/install || exit 1
-	popd || exit 1
-	FNAME="ftnoir-posix-$(date +%Y%m%d-%H_%M_%S)"-"$CURREV".7z
-	7z -y a -t7z -m0=lzma -mx=9 -mfb=128 -md=8m -ms=on "$FNAME" install && foo=1 || exit 1
-	ssh -4o BatchMode=yes "$USER"@"$BACKUPHOST" find /var/www/ftnoir/ -name '"*.7z"' -type f -mtime 14 -delete
-	scp -4o BatchMode=yes "$FNAME" "$USER"@"$BACKUPHOST":/var/www/ftnoir/
-	rm *.7z
-	test $foo && { echo $CURREV > "$ORIGIN"/last-snapshot.rev; }
-	wineserver -k
-fi
+ssh -o BatchMode=yes $WINHOST $WINSH $WINCMD || exit 1
+ssh -o BatchMode=yes $WINHOST $WINSH -c \'$NEKO $WINPATH\' > $TMP/$FNAME || exit 1
+scp -o BatchMode=yes $TMP/$FNAME $USER@$BACKUPHOST:/var/www/ftnoir/
+rm -f $TMP/$FNAME
